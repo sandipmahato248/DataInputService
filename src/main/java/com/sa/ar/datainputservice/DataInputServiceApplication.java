4ff825510c54d5d9cdb7a6d65c9728f43223bfd2
Sandip Mahato <sandip@Sandips-MacBook-Pro.local>
2023-03-18 20:36:28 -0500

    data input service

---
+++ src/main/java/com/sa/ar/datainputservice/DataInputServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sa.ar.datainputservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataInputServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataInputServiceApplication.class, args);
+	}
+
+}
